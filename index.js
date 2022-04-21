56b3a54af35a90449028fd4c10d0fa18cfbf5136
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-04-21 22:52:07 +0200

    12

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ function boton(){
     let texto = document.getElementById('participantes').value;
     let participantes = texto.split(",");
     let premios = new Array;
+    let total = 0;
     for(let i = 0; i < participantes.length; i++){
         premios.push(0);  
     }
     for(let i = 0; i < cofres; i++){
-        let aleatorio =  Math.floor(Math.random()*(participantes.length-1));
+        let aleatorio =  Math.floor(Math.random()*(participantes.length));
         console.log(aleatorio);
         premios[aleatorio]++;
     }
     for(let i = 0; i < participantes.length; i++){
         console.log(participantes[i] + " " + premios[i]);   
+        total+=premios[i];
     }
+    console.log(total);
 };
