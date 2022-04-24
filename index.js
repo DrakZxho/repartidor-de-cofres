81b392d9620ee200ca30500f89295c674da66ddc
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-04-24 15:41:57 +0200

    añadir intervalo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const cofres = document.getElementById('cofres').value * 5;
+const intervalo = document.getElementById('intervalo').value | 10;
+let texto = document.getElementById('participantes').value;
+let participantes = texto.split(",");
+let premios = new Array;
+let resultado = "<table><tr>";
+const aux = Math.trunc(participantes.length / intervalo);
+
 function repartir(){
-    const cofres = document.getElementById('cofres').value * 5;
-    let texto = document.getElementById('participantes').value;
-    let participantes = texto.split(",");
-    let premios = new Array;
-    let resultado = "<table><tr>";
-    const aux = Math.trunc(participantes.length/10);
     for(let i = 0; i < participantes.length; i++){
         premios.push(0);  
     }
@@ -12,15 +14,18 @@ function repartir(){
         let aleatorio =  Math.floor(Math.random()*(participantes.length));
         premios[aleatorio] += 1;
     }
+    mostrar();    
+};
+function mostrar(){
     for(let i = 0; i <= aux; i++){
         resultado += "<th>NOMBRE</th><th>COFRES</th>";
     }
     resultado += "</tr>";
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < intervalo; i++){
         if (participantes[i]){
             resultado += "<tr><td class=\"name\">"+participantes[i]+"</td><td class=\"amount\">"+premios[i]+"</td>";
             for(let j = 1; j <= aux;j++){
-                let aux2 = j*10 + i;
+                let aux2 = j * intervalo + i;
                 if (participantes[aux2]){
                     resultado += "<td class=\"name\">"+participantes[aux2]+"</td><td class=\"amount\">"+premios[aux2]+"</td>";
                 }
