854c00f51ac36e884136acd8eb6092c803871bfc
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-04-24 15:06:50 +0200

    intento de estructura

--- index.js
+++ index.js
@@ -13,22 +13,16 @@ function repartir(){
         premios[aleatorio] += 1;
     }
     for(let i = 0; i <= aux; i++){
-        if(i == 0)
         resultado += "<th>NOMBRE</th><th>COFRES</th>";
-        else resultado += "<th>NOMBRE</th><th>COFRES</th><th></th>";
     }
     resultado += "</tr>";
     for(let i = 0; i < 10; i++){
         if (participantes[i]){
-            if (aux<1)
             resultado += "<tr><td class=\"name\">"+participantes[i]+"</td><td class=\"amount\">"+premios[i]+"</td>";
-            else resultado += "<tr><td class=\"name\">"+participantes[i]+"</td><td class=\"amount\">"+premios[i]+"</td><td></td>";
             for(let j = 1; j <= aux;j++){
                 let aux2 = j*10 + i;
                 if (participantes[aux2]){
-                    if (j < aux)
                     resultado += "<td class=\"name\">"+participantes[aux2]+"</td><td class=\"amount\">"+premios[aux2]+"</td>";
-                    else resultado += "<td class=\"name\">"+participantes[aux2]+"</td><td class=\"amount\">"+premios[aux2]+"</td><td></td>";
                 }
             }
             resultado += "</tr>";
