2034127e33a48ae85c91d4275f1d58f0f0215118
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-04-24 16:47:08 +0200

    añadir intervalo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function repartir(){
     const cofres = document.getElementById('cofres').value * 5;
-    const intervalo = document.getElementById('intervalo').value ? document.getElementById('intervalo').value : 10;
+    const intervalo = (document.getElementById('intervalo').value && document.getElementById('intervalo').value>0) ? document.getElementById('intervalo').value : 10;
     let texto = document.getElementById('participantes').value;
     let participantes = texto.split(",");
     let premios = new Array;
-    let resultado = "<table><tr>";
+    
+
     const aux = Math.trunc(participantes.length / intervalo);
     for(let i = 0; i < participantes.length; i++){
         premios.push(0);  
@@ -13,6 +14,7 @@ function repartir(){
         let aleatorio =  Math.floor(Math.random()*(participantes.length));
         premios[aleatorio] += 1;
     }
+    let resultado = "<table><tr>";
     for(let i = 0; i <= aux; i++){
         resultado += "<th>NOMBRE</th><th>COFRES</th>";
     }
@@ -34,23 +36,4 @@ function repartir(){
     document.getElementById("resultado").innerHTML = resultado;
 };
 function mostrar(){
-    for(let i = 0; i <= aux; i++){
-        resultado += "<th>NOMBRE</th><th>COFRES</th>";
-    }
-    resultado += "</tr>";
-    for(let i = 0; i < intervalo; i++){
-        if (participantes[i]){
-            resultado += "<tr><td class=\"name\">"+participantes[i]+"</td><td class=\"amount\">"+premios[i]+"</td>";
-            for(let j = 1; j <= aux;j++){
-                let aux2 = j * intervalo + i;
-                if (participantes[aux2]){
-                    resultado += "<td class=\"name\">"+participantes[aux2]+"</td><td class=\"amount\">"+premios[aux2]+"</td>";
-                }
-            }
-            resultado += "</tr>";
-        }     
-    }
-    resultado += "</table>";
-    resultado += "<p>Cofres repartidos <strong>" + cofres + "<strong>.</p>"
-    document.getElementById("resultado").innerHTML = resultado;
 };
